255457a8e64498f3d37821c49d570201736c0a64
Sri2614 <sribalaji2614@gmail.com>
2020-11-02 14:26:02 -0500

    Break statement removed

--- 03Intermediate/02 userRoleFunction.js
+++ 03Intermediate/02 userRoleFunction.js
@@ -14,19 +14,14 @@ var getUserRole = function (name, role){
     switch (role) {
         case "admin":
             return `${name} is admin with all access`;
-            break;
         case "subadmin":
             return `${name} is sub-admin with acccess to create/delete courses`
-            break;
         case "testprep":
             return `${name} is a testprep with access to create/delete tests`
-            break;
         case "user":
             return `${name} is a user to consume all content `
-            break;
         default:
             return `${name} is a trial user`
-            break;
     }
 }
 
