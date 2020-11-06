e173ac062653ab398b1c33499c628956f6c23ea0
Sri2614 <sribalaji2614@gmail.com>
2020-11-06 02:35:09 -0500

    Slice and Splice Array Methods

--- 03Intermediate/09 filterArray.js
+++ 03Intermediate/09 filterArray.js
@@ -4,6 +4,13 @@ var testArray = [2, 4, 6, 3, 9, 1, 8];
 
 const myNumbers = [1,3,5,7,2,23,65,33,77,90]
 
-const result = myNumbers.filter((num) => num < 65  )
+// const result = myNumbers.filter((num) => num < 65  )
 
-console.log((result));
+// console.log((result));
+
+
+var users = ["Ted", "Tim", "Ton", "Sam", "Sod"];
+
+// console.log(users.slice(1,4));
+users.splice(1,3,'Sri', "balaji");
+console.log(users);
