b26d352749d30a23e9e835e33c31669c2eef95d6
Sri2614 <sribalaji2614@gmail.com>
2020-11-06 02:01:20 -0500

    Fill and Filter Array methods

---
+++ 03Intermediate/09 filterArray.js
@@ -0,0 +1,9 @@
+var testArray = [2, 4, 6, 3, 9, 1, 8];
+
+// console.log(testArray.fill("s", 1, 4));
+
+const myNumbers = [1,3,5,7,2,23,65,33,77,90]
+
+const result = myNumbers.filter((num) => num < 65  )
+
+console.log((result));
