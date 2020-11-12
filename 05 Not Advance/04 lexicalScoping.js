c17cb0d2a91d78bcc89d1f36806c6d1d710f3922
Sri2614 <sribalaji2614@gmail.com>
2020-11-12 14:09:25 -0500

    Lexical Scoping in JS

---
+++ 05 Not Advance/04 lexicalScoping.js
@@ -0,0 +1,11 @@
+ function init(){
+      var firstName = "Sri";
+      function sayFirstName(){
+          console.log(firstName);
+      }
+      sayFirstName();
+ }
+
+ init();
+
+//  console.log(firstName);
