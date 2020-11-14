738480c172f5b9010bb71e218adced38c8ff0453
Sri2614 <sribalaji2614@gmail.com>
2020-11-14 13:44:23 -0500

    Generating elements and Text node in DOM

---
+++ 05 Not Advance/Project3/script.js
@@ -0,0 +1,65 @@
+const courses = [
+    {
+        name: "Complete JS Course",
+        price: "2.5"
+    },
+    {
+        name: "Complete React JS Course",
+        price: "4.5"
+    },
+    {
+        name: "Complete Angular Course",
+        price: "5.5"
+    },
+    {
+        name: "Complete React Native Course",
+        price: "22.2"
+    },
+    {
+        name: "Complete Flutter Course",
+        price: "4.2"
+    },
+];
+
+function generateList(){
+    const ul = document.querySelector(".list-group");
+    ul.innerHTML = "";
+    courses.forEach( course => {
+        const li = document.createElement("li");
+        li.classList.add("list-group-item");
+
+        const name = document.createTextNode(course.name);
+        li.appendChild(name);
+        
+        const span = document.createElement("span")
+        span.classList.add("float-right");
+        
+        const price = document.createTextNode("$ " + course.price);
+        span.appendChild(price);
+
+        li.appendChild(span);
+        ul.appendChild(li);
+
+
+
+
+    })
+}
+
+// generateList();
+
+window.addEventListener("load", generateList);
+
+
+const button = document.querySelector(".sort-btn");
+
+button.addEventListener("click", () => {
+    courses.sort( (a, b) => a.price - b.price )
+    generateList();
+})
+
+const button2 = document.querySelector(".sort-desc-btn");
+button2.addEventListener("click", () => {
+    courses.sort((a, b) => a.price - b.price).reverse();
+    generateList();
+})
